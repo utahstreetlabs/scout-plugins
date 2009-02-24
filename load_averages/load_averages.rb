c8cfaaa787c9011f9ddd1e14c9447e7d0a5a3e3d
itsderek23 <derek.haynes@gmail.com>
2009-02-24 14:54:21 -0800

    Wrong branch

--- load_averages/load_averages.rb
+++ load_averages/load_averages.rb
@@ -1,24 +1,21 @@
-# 
-# This is a real plugin updated to use our newer interface:
-# 
-#   http://scoutapp.com/plugin_urls/static/creating_a_plugin
-# 
-class LoadAverages < ScoutAgent::Plugin
+class LoadAverages < Scout::Plugin
   TEST_USAGE = "#{File.basename($0)} max_load MAX_LOAD"
-
-   def build_report
-     if `uptime` =~ /load average(s*): ([\d.]+)(,*) ([\d.]+)(,*) ([\d.]+)\Z/
-       report :last_minute          => $2,
-              :last_five_minutes    => $4,
-              :last_fifteen_minutes => $6
-     else
-       raise "Unexpected output format"  
-     end
-     if option(:max_load) and
-        reports.last[:last_minute].to_f > option(:max_load).to_f
-       alert "Maximum Load Exceeded (#{reports.last[:last_minute]})", ""
-     end
-  rescue Exception
-    error "Couldn't use `uptime` as expected.", $!.message
+  
+   def run
+    data = {}
+    data[:alerts] = []
+    if `uptime` =~ /load average(s*): ([\d.]+)(,*) ([\d.]+)(,*) ([\d.]+)\Z/
+      data[:report] = { :last_minute          => $2,
+                        :last_five_minutes    => $4,
+                        :last_fifteen_minutes => $6 }
+    else
+      raise "Unexpected output format"  
+    end
+    if @options['max_load'] and data[:report][:last_minute].to_f > @options['max_load'].to_f
+      data[:alerts] << {:subject => "Maximum Load Exceeded (#{data[:report][:last_minute]})"}
+    end
+    data
+  rescue
+    {:error => {:subject => "Couldn't use `uptime` as expected.", :body => $!.message} }
   end
 end

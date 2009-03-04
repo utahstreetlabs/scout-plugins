355efa6e54a58ccc664b93755ce57edac63a56a2
Andre Lewis <andre@highgroove.com>
2009-03-04 09:36:08 -0800

    updated for new agent

--- memory_usage/memory_usage.rb
+++ memory_usage/memory_usage.rb
@@ -6,51 +6,47 @@ class MemoryUsage < Scout::Plugin
             "m" => 1024 * 1024,
             "g" => 1024 * 1024 * 1024 }
             
-  def run
+  def build_report
     top_output = `top -#{RUBY_PLATFORM.include?('darwin') ? 'l' : 'n'} 1`
     mem        = top_output[/^(?:Phys)?Mem:.+/i] or raise "Missing mem"
     swap       = top_output[/^Swap:.+/i]
-    @report    = {:report => Hash.new, :alerts => Array.new}
+
     report_memory(mem,  "mem")
     report_percent("mem")
     if swap           
       report_memory(swap, "swap")
       report_percent("swap")
-      if swap_used = @report[:report][:swap_used]
-        @report[:report][:swap_ratio] = 
-        ratio                         = ( swap_used.to_f /
-                                          @report[:report][:mem_used] ).round
+      if swap_used = report.last[:swap_used]
+        ratio  = ( swap_used.to_f / report.last[:mem_used] ).round        
+        
+        report(:swap_ratio => ratio)
+
         max = @options["max_swap_used"].to_i*UNITS["m"]
         if max > 0 and swap_used > max
-          report[:alerts] <<
-            {:subject => "Maximum Swap Exceeded (#{swap_used})"}
+          alert "Maximum Swap Exceeded (#{swap_used})", ''
         end
         max = @options["max_swap_ratio"].to_i
         if max > 0 and ratio > max
-          report[:alerts] <<
-            {:subject => "Maximum Swap Ratio Exceeded (#{ratio})"}
+          alert "Maximum Swap Ratio Exceeded (#{ratio})", ''
         end
       end
     end
     @report
   rescue
-    { :error => { :subject => "Couldn't use `top` as expected.",
-                  :body    => $!.message } }
+    error "Couldn't use `top` as expected.", $!.message
   end
   
   private
   
   def report_memory(data, type)
     data.scan(/(\d+|\d+\.\d+)([bkmg])\s+(\w+)/i) do |amount, unit, label|
-      @report[:report]["#{type}_#{label.downcase}".to_sym] =
-        (amount.to_f * UNITS[unit.downcase]).round
+      report( "#{type}_#{label.downcase}".to_sym => (amount.to_f * UNITS[unit.downcase]).round )
     end
   end
   
   def report_percent(type)
-    used = @report[:report]["#{type}_used".to_sym] or return
-    free = @report[:report]["#{type}_free".to_sym] or return
-    @report[:report]["#{type}_used_percent".to_sym] =
-      (used.to_f / (used + free)  * 100).round
+    used = report.last["#{type}_used".to_sym] or return
+    free = report.last["#{type}_free".to_sym] or return
+    report( "#{type}_used_percent".to_sym => (used.to_f / (used + free)  * 100).round )
   end
 end

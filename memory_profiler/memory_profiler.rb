45c7c6cdb3a24336e81d898c89626df34c9f2f9f
itsderek23 <derek.haynes@gmail.com>
2009-03-12 22:26:01 -0700

    First shot at memory profiler, quick cleanup of disk usage

--- memory_profiler/memory_profiler.rb
+++ memory_profiler/memory_profiler.rb
@@ -1,12 +1,16 @@
 class MemoryProfiler < Scout::Plugin
-  def run
+  def build_report
     mem_info = {}
     `cat /proc/meminfo`.each do |line|
       _, key, value = *line.match(/^(\w+):\s+(\d+)\s/)
       mem_info[key] = value.to_i
     end
-
-
+    
+    # memory info is empty - operating system may not support it (why doesn't an exception get raised earlier on mac osx?)
+    if mem_info.empty?
+      raise "No such file or directory"
+    end
+    
     mem_total = mem_info['MemTotal'] / 1024
     mem_free = (mem_info['MemFree'] + mem_info['Buffers'] + mem_info['Cached']) / 1024
     mem_used = mem_total - mem_free
@@ -19,20 +23,16 @@ class MemoryProfiler < Scout::Plugin
       swap_percent_used = (swap_used / swap_total.to_f * 100).to_i
     end
 
-    report = {}
+    report('Memory Total' => mem_total)
+    report('Memory Used' => mem_used)
+    report('% Memory Used' => mem_percent_used)
 
-    report['Memory Total'] = mem_total
-    report['Memory Used'] = mem_used
-    report['% Memory Used'] = mem_percent_used
-
-    report['Swap Total'] = swap_total
-    report['Swap Used'] = swap_used
+    report('Swap Total' => swap_total)
+    report('Swap Used' => swap_used)
     unless  swap_total == 0   
-      report['% Swap Used'] = swap_percent_used
+      report('% Swap Used' => swap_percent_used)
     end
-
-    { :report => report }
-    
+        
   rescue Exception => e
     body = if e.message =~ /No such file or directory/
       %Q(Unable to find /proc/meminfo. Please ensure your operationg system supports procfs:
@@ -40,7 +40,8 @@ class MemoryProfiler < Scout::Plugin
     else
       "An error occurred profiling the memory:\n\n#{e.message}\n\n#{e.backtrace}"
     end
-    {:error => {:subject => "Unable to Profile Memory",
-                :body    => body}}
+    error(:subject => "Unable to Profile Memory",
+          :body    => body
+         )
   end
 end

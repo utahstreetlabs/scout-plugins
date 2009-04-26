59f018b0a7f1d31a785f0f5f00cdc7dad30c22f4
Andre Lewis <andre@highgroove.com>
2009-04-26 12:55:19 -0700

    made agent compatible, added memory and 'stickiness'  within the plugin, removed default triggers

--- url_monitor/url_monitor.rb
+++ url_monitor/url_monitor.rb
@@ -1,7 +1,7 @@
 require 'net/http'
 require 'uri'
 
-class UrlMonitor < Scout::Plugin
+class UrlMonitor < ScoutAgent::Plugin
   include Net
   
   TEST_USAGE = "#{File.basename($0)} url URL last_run LAST_RUN"
@@ -19,14 +19,32 @@ class UrlMonitor < Scout::Plugin
     response = http_response
     report(:status => response.class.to_s)
     
-    if valid_http_response?(response)
-      report(:up => 1)
-    else 
-      report(:up => 0)
-      alert(:subject => "The URL [#{@options['url']}] is not responding",
-            :body => "URL: #{@options['url']}\n\nStatus: #{response.to_s}"
-            )
+    is_up = valid_http_response?(response) ? 1 : 0
+    report(:up => is_up)
+    
+    if is_up != memory(:was_up)
+      if is_up == 0
+        alert(:subject => "The URL [#{@options['url']}] is not responding",
+              :body => "URL: #{@options['url']}\n\nStatus: #{response.to_s}"
+              ) 
+        remember(:down_at => Time.now)
+      else
+        if memory(:was_up) && memory(:down_at)
+          alert(:subject => "The URL [#{@options['url']}] is responding again",
+                :body => "URL: #{@options['url']}\n\nStatus: #{response.to_s}. " +
+                          "Was unresponsive for #{(Time.now - memory(:down_at)).to_i} seconds"
+                )
+        else
+          alert(:subject => "The URL [#{@options['url']}] is responding",
+                :body => "URL: #{@options['url']}\n\nStatus: #{response.to_s}. "
+               )
+        end
+        memory.delete(:down_at)
+            
+      end
     end
+    
+    remember(:was_up => is_up)
   rescue
     error(:subject => "Error monitoring url [#{@options['url']}]",
           :body    => $!.message + '<br\><br\>' + $!.backtrace.join('<br/>'))

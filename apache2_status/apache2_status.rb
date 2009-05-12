2e4a5c0c625b71c01d4738f29520a9065f35e52f
Hampton Catlin <hcatlin@greed.local>
2009-05-13 02:10:53 +0800

    adding in request averaging
    Signed-off-by: Jesse Newland <jnewland@gmail.com>

--- apache2_status/apache2_status.rb
+++ apache2_status/apache2_status.rb
@@ -1,19 +1,35 @@
 class Apache2Status < Scout::Plugin
 
   def build_report
-    results = `apache2ctl status`
-    
-    requests_being_processed = results.match(/([0-9]*) requests currently being processed/)[1].to_i
     report(:requests_being_processed => requests_being_processed)
-    
+    report(:apache_reserved_memory_size => apache_reserved_memory_size)
+  end
+  
+  def apache_reserved_memory_size
     process_list = `ps aux | grep apache2 | grep -v grep | grep -v ruby`.split("\n")
-    
     
     memory_size = 0
     process_list.collect! do |line|
       memory_size += line.split(" ")[4].to_i
     end
     
-    report(:apache_reserved_memory_size => memory_size.to_s + " bytes")
+    memory_size.to_s + " bytes"
+  end
+  
+  def requests_being_processed
+    total_requests_being_processed = 0
+    sample_size = options('sample_size').to_i
+    
+    sample_size.times do
+     total_requests_being_processed += fetch_requests_being_processed
+     sleep(options('sample_sleep').to_f)
+    end
+    
+    average_requests_being_processed = (total_requests_being_processed / sample_size)
+  end
+  
+  def fetch_requests_being_processed
+    results = `apache2ctl status`
+    requests_being_processed = results.match(/([0-9]*) requests currently being processed/)[1].to_i
   end
 end

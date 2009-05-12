b3f106a1e2c1365063fa9ac442a87fb4f7027a3c
itsderek23 <derek.haynes@gmail.com>
2009-05-12 16:50:53 -0700

    Updates to slow queries

--- mysql_slow_queries/mysql_slow_queries.rb
+++ mysql_slow_queries/mysql_slow_queries.rb
@@ -8,7 +8,7 @@ require "time"
 #
 
 class ScoutMysqlSlow < Scout::Plugin
-  def run
+  def build_report
     begin
       require "elif"
     rescue LoadError
@@ -16,14 +16,14 @@ class ScoutMysqlSlow < Scout::Plugin
         require "rubygems"
         require "elif"
       rescue LoadError
-        return { :error => { :subject => "Couldn't load Elif.",
-                             :body    => "The Elif library is required by " +
-                                         "this plugin." } }
+        error :subject => "Couldn't load Elif.",
+              :body    => "The Elif library is required by " +
+                           "this plugin." 
       end
     end
             
-    if @options["mysql_slow_log"].nil? or @options["mysql_slow_log"].strip.length == 0
-      return { :error => { :subject => "A path to the MySQL Slow Query log file wasn't provided." } }
+    if option("mysql_slow_log").nil? or option("mysql_slow_log").strip.length == 0
+      return error(:subject => "A path to the MySQL Slow Query log file wasn't provided.")
     end
 
   
@@ -33,18 +33,13 @@ class ScoutMysqlSlow < Scout::Plugin
     slow_query_count = 0
     
     slow_queries_text = ''
-    report = { :report => { :slow_query_count => 0,
-                            :very_slow_query_count => 0 },
-               :alerts => Array.new }
-
+    report_data = { :slow_query_count => 0,
+                    :very_slow_query_count => 0 }
     
-    max_query_time = @options["max_query_time"].to_i
-    Elif.foreach(@options["mysql_slow_log"]) do |line|
+    max_query_time = option("max_query_time").to_i
+    Elif.foreach(option("mysql_slow_log")) do |line|
       if line =~ /^# Query_time: (\d+) .+$/
-        
         slow_query_count += 1
-
-
         query_time = $1.to_i
         if (max_query_time > 0) and (query_time > max_query_time)
           slow_queries << {:time => query_time, :sql => sql.reverse}
@@ -54,15 +49,15 @@ class ScoutMysqlSlow < Scout::Plugin
       elsif line =~ /^\# Time: (.*)$/
         t = Time.parse($1) {|y| y < 100 ? y + 2000 : y}
         
-        t2 = @last_run || (@options["last_run"] ? Time.parse(@options["last_run"]) : Time.now)
+        t2 = memory(:last_run) || Time.now
         if t < t2
           break
         else
           slow_queries.each do |sq|
             slow_queries_text += "#{sq[:sql]}Took: #{sq[:time]}s\n\n"
           end
-          report[:report][:very_slow_query_count] += very_slow_query_count
-          report[:report][:slow_query_count] += slow_query_count
+          report_data[:very_slow_query_count] += very_slow_query_count
+          report_data[:slow_query_count] += slow_query_count
           
           very_slow_query_count = slow_query_count = 0
         end
@@ -72,15 +67,15 @@ class ScoutMysqlSlow < Scout::Plugin
       end
     end  
     
-    if report[:report] and (count = report[:report][:very_slow_query_count].to_i and count > 0)
-      report[:alerts] << {:subject => "Maximum Query Time (#{@options["max_query_time"].to_s} sec) exceeded on #{count} #{count > 1 ? 'queries' : 'query'}",
-                          :body => slow_queries_text}
+    if report_data and (count = report_data[:very_slow_query_count].to_i and count > 0)
+      alert(:subject => "Maximum Query Time (#{option("max_query_time").to_s} sec) exceeded on #{count} #{count > 1 ? 'queries' : 'query'}",
+            :body => slow_queries_text)
     end
-    
-    return(report)
+    remember(:last_run,Time.now)
+    report(report_data)
 
     rescue
-      { :error => { :subject => "Couldn't parse log file.",
-                    :body    => $!.message } }
+      error(:subject => "Couldn't parse log file.",
+                    :body    => $!.message)
   end
 end

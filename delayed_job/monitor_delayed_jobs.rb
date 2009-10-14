61f716ecfb9c0a2575d05b64179995b9da98bfb6
Overbryd <l.rieder@gmail.com>
2009-10-14 12:47:39 +0200

    langalex+overbryd Added delayed jobs monitoring plugin with specs

---
+++ delayed_job/monitor_delayed_jobs.rb
@@ -0,0 +1,21 @@
+class DelayedJob < ActiveRecord::Base; end
+
+class MonitorDelayedJobs < Scout::Plugin
+  needs 'active_record', 'yaml'
+  def build_report
+    db_config = YAML::load(File.open(@options['path_to_app'] + '/config/database.yml'))
+    ActiveRecord::Base.establish_connection(db_config[@options['rails_env']])
+    
+    report 'No. of Jobs' => DelayedJob.count
+    
+    jobs_by_prio.each do |job|
+      report "Priority #{job.priority} Jobs" => job.count.to_i
+    end
+  end
+  
+  private
+  
+  def jobs_by_prio
+    DelayedJob.all(:select => 'priority, count(1) AS count', :group => 'priority')
+  end
+end

606d2e068ef1fd01cc14e08013604a661971e79b
Andre Lewis <andre@highgroove.com>
2009-11-18 09:57:02 -0800

    brought mongrel_cluster_monitor up to date

--- mongrel_cluster_monitor/mongrel_cluster_monitor.rb
+++ mongrel_cluster_monitor/mongrel_cluster_monitor.rb
@@ -1,8 +1,12 @@
 class MongrelClusterMonitor < Scout::Plugin
-  def run
-    mongrel_configuration_dir = @options["mongrel_cluster_configuration_dir"] || "/etc/mongrel_cluster/"
-    mongrel_rails_command = @options["mongrel_rails_command"] || "mongrel_rails"
-    to_return = {:alerts => [], :report => {}, :memory => {}}
+  def build_report
+    mongrel_configuration_dir = option("mongrel_cluster_configuration_dir") ||  "/etc/mongrel_cluster/"
+    mongrel_rails_command = option("mongrel_rails_command") || "mongrel_rails"
+    res={}
+    if !File.exist?(mongrel_configuration_dir)
+      error(:subject=>"mongrel_configuration_dir: #{mongrel_configuration_dir} does not exist -- check options")
+      return
+    end
     Dir.chdir(mongrel_configuration_dir) do
       configs = Dir.glob("*.{yml,conf}")
 
@@ -13,26 +17,24 @@ class MongrelClusterMonitor < Scout::Plugin
           if mongrel_status.empty? 
             raise "mongrel_rails command: `#{mongrel_rails_command}` not found or no status information available"
           elsif mongrel_status.include?("missing")
-            if @memory[application_name]
-              to_return[:alerts] << {:subject => "#{application_name} is still down. Attempting Start."}
+            if memory(application_name)
+              alert(:subject=>"Still down: one  or more mongrels for #{application_name}. Attempting Start.", :body=>mongrel_status)
               mongrel_start = `#{mongrel_rails_command} cluster::start -C #{mongrel_configuration_dir}/#{config}`
             else
-              to_return[:alerts] << {:subject => "#{application_name} is down."}
-              to_return[:memory][application_name] = Time.now
+              alert(:subject => "Down: one or more mongrels for #{application_name}", :body=>mongrel_status)
+              remember(application_name,Time.now)
             end
-            to_return[:report][application_name] = 0
+            res[application_name] = 0
           else
-            to_return[:report][application_name] = 1
-            to_return[:memory][application_name] = nil
+            res[application_name] = 1
+            remember(application_name,nil)
           end
         end
       else
-        to_return[:alerts] << {:subject => "No mongrel configuration files found."}
+        alert(:subject => "No mongrel configuration files found.")
       end
     end
-    return to_return  
-  rescue Exception
-    { :error => { :subject => "Couldn't monitor the server.",
-                  :body    => "An exception was thrown:  #{$!.message}" } }
+    report(res)
+
   end
 end

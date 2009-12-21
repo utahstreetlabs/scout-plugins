492be2e33ceffb8d791725bac48dbb8a28b7ffa0
Eric Lindvall <eric@5stops.com>
2009-12-21 02:10:28 -0800

    Update iostat plugin to no longer use iostat.
    Now uses /proc filesystem to gather statistics directly.
    
    This means the statistics will be for the entire sample size, not just 3-5
    seconds of it.

--- iostat/iostat.rb
+++ iostat/iostat.rb
@@ -1,40 +1,80 @@
 class Iostat < Scout::Plugin
   def build_report
-    
-    # Using the second reading- avg since previous check
-    stats = iostat_output.grep(/#{device}/).last.split
-    
-    # Expected output format: 
-    #Device:         rrqm/s   wrqm/s     r/s     w/s    rkB/s    wkB/s avgrq-sz avgqu-sz   await  svctm  %util
-    #xvda1             0.00     0.00    0.00    0.00     0.00     0.00     0.00     0.00    0.00   0.00   0.00 
-    report(
-      :rps     => stats[3], 
-      :wps     => stats[4],
-      :rkbps   => stats[5],
-      :wkbps   => stats[6],
-      :await   => stats[9],
-      :util    => stats[11]
-    )
-    
-  rescue Exception => e
-    error "Couldn't parse output. Make sure you have iostat installed. #{e}"
-    logger.error e
-    logger.error "Output: #{iostat_output}"
+    stats = iostat(device)
+
+    counter(:rps,   stats['rio'],        :per => :second)
+    counter(:rps,   stats['wio'],        :per => :second)
+    counter(:rkbps, stats['rsect'] / 2,  :per => :second)
+    counter(:wkbps, stats['wsect'] / 2,  :per => :second)
+    counter(:util,  stats['use'] / 10.0, :per => :second)
+
+    if old = memory("#{device}_stats")
+      ios = (stats['rio'] - old['rio']) + (stats['wio']  - old['wio'])
+
+      if ios > 0
+        await = ((stats['ruse'] - old['ruse']) + (stats['wuse'] - old['wuse'])) / ios.to_f
+
+        report(:await => await)
+      end
+    end
+
+    remember("#{device}_stats" => stats)
   end
   
   private
+  COLUMNS = %w(major minor name rio rmerge rsect ruse wio wmerge wsect wuse running use aveq)
 
-  def iostat_output
-    @output ||= lambda do
-      command = option('command') || 'iostat -dxk'
-      interval = option('interval') || 3
-      iostat_command = "#{command} #{interval} 2"
-      `#{iostat_command}`
-    end.call
+  def iostat(dev)
+    IO.foreach('/proc/diskstats') do |line|
+      entry = Hash[*COLUMNS.zip(line.strip.split(/\s+/).collect { |v| Integer(v) rescue v }).flatten]
+
+      return entry if entry['name'] == dev
+    end
+
+    nil
   end
-  
+
   def device
     option('device') || `mount`.grep(/ \/ /)[0].split[0].split('/').last
   end
 
+  # Would be nice to be part of scout internals
+  def counter(name, value, options = {}, &block)
+    current_time = Time.now
+
+    if data = memory(name)
+      last_time, last_value = data[:time], data[:value]
+      elapsed_seconds       = current_time - last_time
+
+      # We won't log it if the value has wrapped or enough time hasn't
+      # elapsed
+      if value >= last_value && elapsed_seconds >= 1
+        if block
+          result = block.call(last_value, value)
+        else
+          result = value - last_value
+        end
+
+        case options[:per]
+        when :second, 'second'
+          result = result / elapsed_seconds.to_f
+        when :minute, 'minute'
+          result = result / elapsed_seconds.to_f / 60.0
+        else
+          raise "Unknown option for ':per': #{options[:per].inspect}"
+        end
+
+        if options[:round]
+          # Backward compatibility
+          options[:round] = 1 if options[:round] == true
+
+          result = (result * (10 ** options[:round])).round / (10 ** options[:round]).to_f
+        end
+
+        report(name => result)
+      end
+    end
+
+    remember(name => { :time => current_time, :value => value })
+  end
 end

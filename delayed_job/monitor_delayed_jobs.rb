80c8521f1e7a697913e40af203afa1f7ccb627b4
Eric Lindvall <eric@5stops.com>
2010-01-12 12:37:06 -0800

    Add "Scheduled" metric to delayed_job and ensure that "Oldest waiting" time is always returned.

--- delayed_job/monitor_delayed_jobs.rb
+++ delayed_job/monitor_delayed_jobs.rb
@@ -10,14 +10,17 @@ class MonitorDelayedJobs < Scout::Plugin
     db_config = YAML::load(File.open(@options['path_to_app'] + '/config/database.yml'))
     ActiveRecord::Base.establish_connection(db_config[@options['rails_env']])
     
-    report :total   => DelayedJob.count
-    report :running => DelayedJob.count(:conditions => 'locked_at IS NOT NULL')
-    report :waiting => DelayedJob.count(:conditions => [ 'run_at <= ? AND locked_at IS NULL', Time.now.utc ])
-    report :failing => DelayedJob.count(:conditions => 'attempts > 0')
-    report :failed  => DelayedJob.count(:conditions => 'failed_at IS NOT NULL')
+    report :total     => DelayedJob.count
+    report :running   => DelayedJob.count(:conditions => 'locked_at IS NOT NULL')
+    report :waiting   => DelayedJob.count(:conditions => [ 'run_at <= ? AND locked_at IS NULL', Time.now.utc ])
+    report :scheduled => DelayedJob.count(:conditions => [ 'run_at > ? AND locked_at IS NULL', Time.now.utc ])
+    report :failing   => DelayedJob.count(:conditions => 'attempts > 0')
+    report :failed    => DelayedJob.count(:conditions => 'failed_at IS NOT NULL')
     
     if oldest = DelayedJob.find(:first, :conditions => [ 'run_at <= ? AND locked_at IS NULL', Time.now.utc ], :order => :id)
       report :oldest => (Time.now.utc - oldest.created_at) / 60
+    else
+      report :oldest => 0
     end
   end
 end

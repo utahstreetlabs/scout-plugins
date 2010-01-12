59c295bef8a66b894d224c53e1c90dd191a8b97c
Eric Lindvall <eric@5stops.com>
2010-01-12 12:54:09 -0800

    Improving conditions for metrics in delayed_job.

--- delayed_job/monitor_delayed_jobs.rb
+++ delayed_job/monitor_delayed_jobs.rb
@@ -10,14 +10,21 @@ class MonitorDelayedJobs < Scout::Plugin
     db_config = YAML::load(File.open(@options['path_to_app'] + '/config/database.yml'))
     ActiveRecord::Base.establish_connection(db_config[@options['rails_env']])
     
+    # ALl jobs
     report :total     => DelayedJob.count
+    # Jobs that are currently being run by workers
     report :running   => DelayedJob.count(:conditions => 'locked_at IS NOT NULL')
-    report :waiting   => DelayedJob.count(:conditions => [ 'run_at <= ? AND locked_at IS NULL', Time.now.utc ])
-    report :scheduled => DelayedJob.count(:conditions => [ 'run_at > ? AND locked_at IS NULL', Time.now.utc ])
-    report :failing   => DelayedJob.count(:conditions => 'attempts > 0')
+    # Jobs that are ready to run but haven't ever been run
+    report :waiting   => DelayedJob.count(:conditions => [ 'run_at <= ? AND locked_at IS NULL AND attempts = 0', Time.now.utc ])
+    # Jobs that haven't ever been run but are not set to run until later
+    report :scheduled => DelayedJob.count(:conditions => [ 'run_at > ? AND locked_at IS NULL AND attempts = 0', Time.now.utc ])
+    # Jobs that aren't running that have failed at least once
+    report :failing   => DelayedJob.count(:conditions => 'attempts > 0 AND failed_at IS NULL AND locked_at IS NULL')
+    # Jobs that have permanently failed
     report :failed    => DelayedJob.count(:conditions => 'failed_at IS NOT NULL')
     
-    if oldest = DelayedJob.find(:first, :conditions => [ 'run_at <= ? AND locked_at IS NULL', Time.now.utc ], :order => :id)
+    # The oldest job that hasn't yet been run, in minutes
+    if oldest = DelayedJob.find(:first, :conditions => [ 'run_at <= ? AND locked_at IS NULL AND attempts = 0', Time.now.utc ], :order => :id)
       report :oldest => (Time.now.utc - oldest.created_at) / 60
     else
       report :oldest => 0

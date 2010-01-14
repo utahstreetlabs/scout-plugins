bb81c8a2966168d903f6dcc5f9a9dde8f5f44a21
Ches Martin <ches@whiskeyandgrits.net>
2010-01-14 19:45:26 +0700

    Use redis client library instead of redis-cli.
    We're using a Debian package that doesn't include redis-cli, nor is
    there a separate package for it, so the gem gets the job done.

--- redis-info/redis-info.rb
+++ redis-info/redis-info.rb
@@ -1,18 +1,19 @@
-require 'yaml'
+class RedisMonitor < Scout::Plugin
+  needs 'redis', 'yaml'
 
-class Redis < Scout::Plugin
   KILOBYTE = 1024
   MEGABYTE = 1048576
 
   def build_report
-    info = YAML.load(%x{redis-cli info}.gsub(/:/, ": "))
+    redis = Redis.new
+    info  = redis.info
 
-    report('used_memory_in_kb' => info['used_memory'] / KILOBYTE)
-    report('used_memory_in_mb' => info['used_memory'] / MEGABYTE)
+    report(:used_memory_in_kb => info[:used_memory].to_i / KILOBYTE)
+    report(:used_memory_in_mb => info[:used_memory].to_i / MEGABYTE)
 
     # General Stats
-    %w(used_memory changes_since_last_save).each do |key|
-      report(key => info[key])
+    %w(used_memory changes_since_last_save uptime_in_seconds connected_clients connected_slaves bgsave_in_progress).each do |key|
+      report(key => info[key.intern])
     end
   end
 end

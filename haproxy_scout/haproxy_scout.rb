be77ac92e418839b7237c40f6c9f0da492fb2826
Jesse Newland <jnewland@gmail.com>
2010-02-01 22:54:10 -0500

    only report on frontend and backends, include rates

--- haproxy_scout/haproxy_scout.rb
+++ haproxy_scout/haproxy_scout.rb
@@ -3,15 +3,11 @@
 class HaproxyStats < Scout::Plugin
   def build_report
     FasterCSV.parse(open(option(:uri)), :headers => true) do |row|
-      name = row["# pxname"] + ' ' + row["svname"].downcase
-      report "#{name} Current Sessions" => row["scur"]
-      report "#{name} Current Queue" => row["qcur"] unless row["qcur"].nil?
-      if row['status'] == 'DOWN'
-        alert("#{row['svname']} DOWN", row.to_hash.inspect) unless memory("#{row['svname']} DOWN")
-        remember("#{row['svname']} DOWN" => true)
-      elsif memory("#{row['svname']} DOWN")
-        alert("#{row['svname']} UP", row.to_hash.inspect)
-        memory.delete("#{row['svname']} DOWN")
+      if row["svname"] == 'FRONTEND' || row["svname"] == 'BACKEND'
+        name = row["# pxname"] + ' ' + row["svname"].downcase
+        report "#{name} Current Sessions" => row["scur"]
+        report "#{name} Current Queue" => row["qcur"] unless row["qcur"].nil?
+        report "#{name} Requests / second" => row["rate"] unless row["rate"].nil?
       end
     end
   end

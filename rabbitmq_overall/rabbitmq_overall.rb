afd7761559b522868b57ac9d73a76c0765b20dfd
itsderek23 <derek.haynes@gmail.com>
2010-03-05 15:09:11 -0800

    Added RabbitMQ Monitoring Plugins

---
+++ rabbitmq_overall/rabbitmq_overall.rb
@@ -0,0 +1,63 @@
+# Created by Doug Barth. 
+# http://github.com/dougbarth/
+class RabbitmqOverall < Scout::Plugin
+  OPTIONS = <<-EOS
+  rabbitmqctl:
+    name: rabbitmqctl command
+    notes: The command used to run the rabbitctl program, minus arguments
+    default: rabbitmqctl
+  EOS
+
+  def build_report
+    begin
+      report_data = {}
+
+      connection_stats = `#{rabbitmqctl} -q list_connections`.to_a
+      report_data['connections'] = connection_stats.size
+
+      report_data['queues'] = report_data['messages'] = report_data['queue_mem'] = 0
+      report_data['exchanges'] = 0
+      report_data['bindings'] = 0
+      vhosts.each do |vhost|
+        queue_stats = `#{rabbitmqctl} -q list_queues -p '#{vhost}' messages memory`.to_a
+        report_data['queues'] += queue_stats.size
+        report_data['messages'] += queue_stats.inject(0) do |sum, line|
+          sum += line.split[0].to_i
+        end
+
+        report_data['queue_mem'] += queue_stats.inject(0) do |sum, line|
+          sum += line.split[1].to_i
+        end
+
+        exchange_stats = `#{rabbitmqctl} -q list_exchanges -p #{vhost}`.to_a
+        report_data['exchanges'] += exchange_stats.size
+
+        binding_stats = `#{rabbitmqctl} -q list_bindings -p #{vhost}`.to_a
+        report_data['bindings'] += binding_stats.size
+      end
+
+      # Convert queue memory from bytes to MB.
+      report_data['queue_mem'] = report_data['queue_mem'].to_f / (1024 * 1024)
+
+      report(report_data)
+    rescue RuntimeError => e
+      add_error(e.message)
+    end
+  end
+
+  def rabbitmqctl
+    option('rabbitmqctl')
+  end
+
+  def vhosts
+    @vhosts ||= `#{rabbitmqctl} -q list_vhosts`.to_a
+  end
+
+  def `(command)
+    result = super(command)
+    if ($? != 0)
+      raise "<#{command}> exited with a non-zero value: #{$?}"
+    end
+    result
+  end
+end
---
+++ rabbitmq_queue_details/rabbitmq_queue_details.rb
@@ -0,0 +1,64 @@
+# Created by Doug Barth. 
+# http://github.com/dougbarth/
+class RabbitmqOverall < Scout::Plugin
+  OPTIONS = <<-EOS
+  rabbitmqctl:
+    name: rabbitmqctl command
+    notes: The command used to run the rabbitctl program, minus arguments
+    default: rabbitmqctl
+  queue:
+    name: Queue
+    notes: The name of the queue to collect detailed metrics for
+  vhost:
+    name: Virtual host
+    notes: The name of the virtual host to collect detailed metrics for
+    default: /
+  EOS
+
+  QUEUE_INFO_ITEMS = %w(name messages_ready messages_unacknowledged messages_uncommitted messages acks_uncommitted consumers transactions memory)
+
+  def build_report
+    rabbitmqctl_script = option('rabbitmqctl')
+    queue_name = option('queue')
+    vhost = option('vhost')
+
+    unless queue_name
+      error("Queue name not specified", "You must specify the queue to get details for.")
+      return
+    end
+
+    queue_stats_line = get_queue_stats_line(rabbitmqctl_script, queue_name, vhost)
+
+    unless queue_stats_line
+      error("\"#{queue_name}\" queue not found", "Please check the queue name for potential errors.")
+      return
+    end
+
+    report(extract_stats(queue_stats_line))
+  end
+
+  private
+    def get_queue_stats_line(rabbitmqctl_script, queue_name, vhost)
+      cmd = vhost.nil? ? "#{rabbitmqctl_script} -q list_queues " : "#{rabbitmqctl_script} -q list_queues -p '#{vhost}' "
+      all_queue_stats = `#{cmd} #{QUEUE_INFO_ITEMS.join(' ')}`.to_a
+      all_queue_stats.detect do |line|
+        line.split[0] == queue_name
+      end
+    end
+
+    def extract_stats(queue_stats_line)
+      queue_stats = queue_stats_line.split
+
+      report_data = {}
+      QUEUE_INFO_ITEMS.each_with_index do |item, i|
+        next if item == 'name'
+        report_data[item] = queue_stats[i]
+
+        if item == 'memory'
+          # Convert from bytes to megabytes
+          report_data[item] = report_data[item].to_f / (1024 * 1024)
+        end
+      end
+      report_data
+    end
+end

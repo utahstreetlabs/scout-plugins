e002c72521a029ebdbbaffb4e6ff7b7568db3e8e
Andre Lewis <andre@highgroove.com>
2010-03-26 15:04:35 -0700

    ruby 1.9 friednly + minor cleanup

--- mpstat/mpstat.rb
+++ mpstat/mpstat.rb
@@ -6,7 +6,7 @@ class MPstat < Scout::Plugin
     notes: The command used to display MP statistics
     default: mpstat
   interval:
-    name: iostat Interval
+    name: mpstat Interval
     notes: Report current usage as the average over this many seconds.
     default: 5
   EOS
@@ -18,9 +18,7 @@ class MPstat < Scout::Plugin
     [:user, :nice, :sys, :iowait, :irq, :soft, :steal, :idle, :intrps].each{|k| result[k]=values[k]}
     report(result)
   rescue Exception => e
-    error "Couldn't parse output. Make sure you have iostat installed. #{e}"
-    # log.error e
-    # log.error "Output: #{output}"
+    error "Couldn't parse output. Make sure you have mpstat installed. #{e}"
   end
 
   private
@@ -29,7 +27,6 @@ class MPstat < Scout::Plugin
     command = option('command') || 'mpstat'
     interval = option('interval') || 5
     stat_command = "#{command} #{interval} 2"
-    # log.info "running iostat_output for #{iostat_command}"
     `#{stat_command}`
   end
 
@@ -39,10 +36,8 @@ class MPstat < Scout::Plugin
     # 04:38:34 PM  all    6.69    0.02    1.30    0.31    0.02    0.13    0.00   91.53    349.37
 
     # take the format fields
-    # log.info "extracting output format"
-    format=output.grep(/CPU/).last.gsub(/\//,'p').gsub(/(%|:)/,'').downcase.split
+    format=output.split("\n").grep(/CPU/).last.gsub(/\//,'p').gsub(/(%|:)/,'').downcase.split
 
-    # log.info "extracting output average values"
     # take all the stat fields
     raw_stats=output.split("\n").grep(/[0-9]+\.[0-9]+$/).last.split
 

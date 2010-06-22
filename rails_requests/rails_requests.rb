ee76776d2be3024eb592931f3f99e33678d3108c
itsderek23 <derek.haynes@gmail.com>
2010-06-22 10:58:09 -0700

    Initial work - need to get RLA gem w/Oink working locally

--- rails_requests/rails_requests.rb
+++ rails_requests/rails_requests.rb
@@ -124,12 +124,12 @@ class RailsRequests < Scout::Plugin
     end
   end
   
-  # Set the RLA rails format to +@file_format+ and inits the log parser (@log_parser). If none provided, defaults to railsoink (which is v2).
+  # Set the RLA rails format to +@file_format+ and inits the log parser (@log_parser). If none provided, defaults to Oink (which is v2).
   def init_parser
     set_file_format_class
     # will use minimal line collection for incremental parsing. using more line collections increases
     # parsing time. 
-    if @file_format_class == RequestLogAnalyzer::FileFormat::RailsOink
+    if @file_format_class == RequestLogAnalyzer::FileFormat::Oink
       @file_format = @file_format_class.create('minimal')  
     else # Rails3 doesn't have option of minimal processing
       @file_format = @file_format_class.create
@@ -144,12 +144,12 @@ class RailsRequests < Scout::Plugin
     rails_version = rails_version.to_s[0..0].to_i
     @file_format_class = case rails_version
               when 2
-                RequestLogAnalyzer::FileFormat::RailsOink
+                RequestLogAnalyzer::FileFormat::Oink
               when 3
                 # TODO - Add Oink processing for Rails3 files
                 RequestLogAnalyzer::FileFormat::Rails3
               else
-                RequestLogAnalyzer::FileFormat::RailsOink
+                RequestLogAnalyzer::FileFormat::Oink
               end
   end
   
@@ -628,82 +628,4 @@ class File
     # We have more data now, so try again to read a line...
     gets(sep_string)
   end  
-end
-
-require "request_log_analyzer"
-
-RequestLogAnalyzer::FileFormat.load(:rails)
-
-class RequestLogAnalyzer::FileFormat::RailsOink < RequestLogAnalyzer::FileFormat::Rails
-  
-  # capture the PID
-  HODEL3000_PROCESSING = /\[(\d+)\]: Processing ((?:\w+::)*\w+)#(\w+)(?: to (\w+))? \(for (#{ip_address}) at (#{timestamp('%Y-%m-%d %H:%M:%S')})\) \[([A-Z]+)\]/
-  
-  LINE_DEFINITIONS[:processing] = RequestLogAnalyzer::LineDefinition.new(:processing, :header => true,
-          :teaser   => /Processing /,
-          :regexp   => Regexp.union(LINE_DEFINITIONS[:processing].regexp,HODEL3000_PROCESSING),
-          :captures => [{ :name => :controller, :type  => :string }, # Default Rails Processing
-                        { :name => :action,     :type  => :string },
-                        { :name => :format,     :type  => :string, :default => 'html' },
-                        { :name => :ip,         :type  => :string },
-                        { :name => :timestamp,  :type  => :timestamp },
-                        { :name => :method,     :type  => :string },
-                        { :name => :pid, :type => :integer },        # Hodel 3000 Processing w/PID
-                        { :name => :controller, :type  => :string },
-                        { :name => :action,     :type  => :string },
-                        { :name => :format,     :type  => :string, :default => 'html' },
-                        { :name => :ip,         :type  => :string },
-                        { :name => :timestamp,  :type  => :timestamp },
-                        { :name => :method,     :type  => :string }])
-  
-  # capture the memory usage
-  LINE_DEFINITIONS[:memory_usage] = RequestLogAnalyzer::LineDefinition.new(:memory_usage,
-   :regexp   => /\[(\d+)\]: Memory usage: (\d+) /,
-   :captures => [{ :name => :pid, :type => :integer },{ :name => :memory, :type => :traffic }])
-  
-  LINE_COLLECTIONS.each { |k,v| LINE_COLLECTIONS[k] << :memory_usage }
-  
-  
-  report(:append) do |analyze|
-      analyze.traffic :memory_diff, :category => REQUEST_CATEGORIZER, :title => "Largest Memory Increases", :line_type => :memory_usage
-  end
-   
-  def pids
-    @pids ||= {}
-  end
-  
-  # override the default Request class for this file format
-  class Request #< RequestLogAnalyzer::FileFormat::Rails::Request
-   def validate
-     update_pids
-   end
- 
-   def update_pids
-     # memory isn't recorded with exceptions. need to set #last_memory_reading+ to -1 as
-     # the memory used could have changed. for the next request the memory change will not be recorded.
-     #
-     # NOTE - the failure regex was not matching with a Rails Development log file.
-     if has_line_type?(:failure) and processing = has_line_type?(:processing)
-       file_format.pids[processing[:pid]][:last_memory_reading] = -1
-     elsif mem_line = has_line_type?(:memory_usage)
-        pid,memory_reading = mem_line.values_at(:pid,:memory)
-        file_format.pids[pid] ||= { :last_memory_reading => -1, :current_memory_reading => -1 }
-        file_format.pids[pid][:current_memory_reading] = memory_reading
-        # calcuate the change in memory
-        unless file_format.pids[pid][:current_memory_reading] == -1 || file_format.pids[pid][:last_memory_reading] == -1
-          # logged as kB, need to convert to bytes for the :traffic Tracker
-          memory_diff = (file_format.pids[pid][:current_memory_reading] - file_format.pids[pid][:last_memory_reading])*1024
-          if memory_diff > 0
-            self.attributes[:memory_diff] = memory_diff
-          end # if memory_diff > 0
-        end # unless
-        
-        file_format.pids[pid][:last_memory_reading] = file_format.pids[pid][:current_memory_reading]
-        file_format.pids[pid][:current_memory_reading] = -1
-      end # if mem_line
-      return true
-   end # def update_pids
-  end # class Request
-   
-   
 end

243f5ef60afc9b803935fa97b3e0132c59ffbf94
itsderek23 <derek.haynes@gmail.com>
2010-06-22 12:56:53 -0700

    Added, updated Postgres plugin

--- postgresql_monitoring/postgresql_monitoring.rb
+++ postgresql_monitoring/postgresql_monitoring.rb
@@ -40,14 +40,13 @@ class PostgresqlMonitoring< Scout::Plugin
                                  sum(n_tup_upd) AS "rows_update",
                                  sum(n_tup_del) AS "rows_delete",
                                  (sum(idx_tup_fetch) + sum(seq_tup_read) + sum(n_tup_ins) + sum(n_tup_upd) + sum(n_tup_del)) AS "rows_total"
-                          FROM pg_stat_user_tables;')
-    
+                          FROM pg_stat_all_tables;')
     row = result[0]
     row.each do |name, val|
       if NON_COUNTER_ENTRIES.include?(name)
         report[name] = val.to_i
       else
-        report[name] = calculate_counter(now, name, val.to_i)
+        counter(name,val.to_i,:per => :second)
       end
     end
 
@@ -63,7 +62,7 @@ class PostgresqlMonitoring< Scout::Plugin
       if NON_COUNTER_ENTRIES.include?(name)
         report[name] = val.to_i
       else
-        report[name] = calculate_counter(now, name, val.to_i)
+        counter(name, val.to_i, :per => :second)
       end
     end
     
@@ -74,27 +73,5 @@ class PostgresqlMonitoring< Scout::Plugin
     end
 
     report(report) if report.values.compact.any?
-  end
-
-  private
-  def calculate_counter(current_time, name, value)
-    result = nil
-    # only check if a past run has a value for the specified query type
-    if memory(name) && memory(name).is_a?(Hash)
-      last_time, last_value = memory(name).values_at('time', 'value')
-      # We won't log it if the value has wrapped
-      if last_value and value >= last_value
-        elapsed_seconds = current_time - last_time
-        elapsed_seconds = 1 if elapsed_seconds < 1
-        result = value - last_value
-
-        # calculate per-second
-        result = result / elapsed_seconds.to_f
-      end
-    end
-
-    remember(name => {:time => current_time, :value => value})
-
-    result
   end
 end

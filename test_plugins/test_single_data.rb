8095960f6ecf068af2b24c55757f7e3914d2edf6
Andre Lewis <andre@highgroove.com>
2010-07-08 15:32:06 -0700

    plugin repo cleanup -- archived old plugins and deleted some

--- test_plugins/test_single_data.rb
+++
@@ -1,7 +0,0 @@
-class TestSingleAlert < Scout::Plugin
-  def run
-    {:report=>{"fish" => "sticks"}, 
-     :alert=>{:subject => "A single Alert", :body => "This is a single alert."},
-     :error=>{:subject => "The roof", :body => "The roof is on fire."}}
-  end
-end

c5b0e4c6346bbab600ba00d122069b4fb3f8c0c6
Andre Lewis <andre@highgroove.com>
2010-08-03 23:26:56 -0700

    changed wording

--- environment_check/environment_check.rb
+++ environment_check/environment_check.rb
@@ -4,7 +4,7 @@ class EnvironmentCheck < Scout::Plugin
     last_run = memory(:last_run)
     # run tuner if it's never run before, or if it's been X days since last tuner run
     if last_run == nil || (last_run.is_a?(Time) && Time.now-86400 >= last_run) # runs once a day max
-      s=["This trouble-shooting plugin runs once a day, and generates an alert like this one each time it runs. To stop getting this alert, remove or disable the EnvironmentCheck plugin.",
+      s=["This alert is generated daily. To stop getting this alert, remove or disable the EnvironmentCheck plugin.",
       "Ruby Version: #{RUBY_VERSION} #{RUBY_PLATFORM} #{RUBY_RELEASE_DATE}",
       "$PATH: \n#{ENV['PATH'].gsub(":","\n")}",
       "Gem Path: #{Gem.path}"].join("\n\n")

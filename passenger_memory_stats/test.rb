8011badd330faf6f1b799077456bf7e51b34e055
Chris Griego <cgriego@gmail.com>
2010-12-08 19:04:40 -0600

    Change Passenger Memory Stats plugin to report only on the server being used, Nginx or Apache

--- passenger_memory_stats/test.rb
+++ passenger_memory_stats/test.rb
@@ -1,36 +1,81 @@
 require File.expand_path('../../test_helper.rb', __FILE__)
 require File.expand_path('../passenger_memory_stats.rb', __FILE__)
 
-
 class PassengerMemoryStatsTest < Test::Unit::TestCase
+  def test_nginx_only
+    stub_command_with FIXTURES[:nginx_only]
+    assert_equal([{
+      "nginx_private_total"     => "26.0 MB",
+      "nginx_processes"         => "5",
+      "nginx_vmsize_total"      => "271.6 MB",
+      "passenger_private_total" => "2251.4 MB",
+      "passenger_processes"     => "16",
+      "passenger_vmsize_total"  => "4377.8 MB",
+    }], PassengerMemoryStats.new(nil, {}, {}).run[:reports])
+  end
 
-    # Stub the plugin instance where necessary and run
-    # @plugin=PluginName.new(last_run, memory, options)
-    #                        date      hash    hash
-  def test_normal
-    PassengerMemoryStats.any_instance.expects(:`).with("passenger-memory-stats 2>&1").returns(FIXTURES[:valid]).once
+  def test_apache_only
+    stub_command_with FIXTURES[:apache_only]
+    assert_equal([{
+      "apache_private_total"    => "7.6 MB",
+      "apache_processes"        => "4",
+      "apache_vmsize_total"     => "1083.3 MB",
+      "passenger_private_total" => "214.8 MB",
+      "passenger_processes"     => "6",
+      "passenger_vmsize_total"  => "636.3 MB",
+    }], PassengerMemoryStats.new(nil, {}, {}).run[:reports])
+  end
+
+  protected
+
+  def stub_command_with(fixture)
+    PassengerMemoryStats.any_instance.expects(:`).with("passenger-memory-stats 2>&1").returns(fixture).once
     `echo''>/dev/null` # total hack: the plugin checks $? (child process exit status) -- this sets $?
-    @plugin=PassengerMemoryStats.new(nil,{},{})
-    res = @plugin.run()
-    assert_valid_report(res)
   end
 
-  private
-  def assert_valid_report(res)
-    assert_equal [{"apache_private_total" => "7.6 MB",
-                   "apache_processes" => "4",
-                   "passenger_processes" => "6",
-                   "passenger_vmsize_total" => "636.3 MB",
-                   "nginx_vmsize_total" => "0.0 MB",
-                   "nginx_processes" => "0",
-                   "passenger_private_total" => "214.8 MB",
-                   "nginx_private_total" => "0.0 MB",
-                   "apache_vmsize_total" => "1083.3 MB"}], res[:reports]
-  end
+  FIXTURES = YAML.load(<<-EOS)
+    :nginx_only: |
+      ------- Apache processes --------
+
+      ### Processes: 0
+      ### Total private dirty RSS: 0.00 MB
 
 
-  FIXTURES=YAML.load(<<-EOS)
-    :valid: |
+      ---------- Nginx processes ----------
+      PID    PPID   VMSize   Private  Name
+      -------------------------------------
+      17881  1      49.8 MB  0.6 MB   nginx: master process /usr/sbin/nginx -c /etc/nginx/nginx.conf
+      18419  17881  54.3 MB  5.2 MB   nginx: worker process
+      18516  17881  54.4 MB  5.2 MB   nginx: worker process
+      18556  17881  58.3 MB  9.2 MB   nginx: worker process
+      18557  17881  54.8 MB  5.8 MB   nginx: worker process
+      ### Processes: 5
+      ### Total private dirty RSS: 26.05 MB
+
+
+      ----- Passenger processes ------
+      PID    VMSize    Private   Name
+      --------------------------------
+      18381  160.9 MB  7.9 MB    PassengerNginxHelperServer /usr/lib/passenger ruby 3 4 0 40 0 3600 1 deploy 1000 1000 /tmp/passenger.17881
+      18391  39.9 MB   8.4 MB    Passenger spawn server
+      18632  308.8 MB  171.5 MB  Rails: /data/www/current
+      18634  313.6 MB  176.0 MB  Rails: /data/www/current
+      499    312.1 MB  173.5 MB  Rails: /data/www/current
+      505    313.9 MB  175.3 MB  Rails: /data/www/current
+      509    312.3 MB  173.5 MB  Rails: /data/www/current
+      23732  296.4 MB  157.5 MB  Rails: /data/www/current
+      23734  298.5 MB  159.9 MB  Rails: /data/www/current
+      23736  305.5 MB  167.0 MB  Rails: /data/www/current
+      23738  302.9 MB  164.0 MB  Rails: /data/www/current
+      23740  302.6 MB  163.8 MB  Rails: /data/www/current
+      23742  295.2 MB  156.2 MB  Rails: /data/www/current
+      2982   230.0 MB  90.6 MB   Passenger ApplicationSpawner: /data/www/current
+      3001   293.2 MB  154.2 MB  Rails: /data/www/current
+      3191   292.0 MB  152.1 MB  Rails: /data/www/current
+      ### Processes: 16
+      ### Total private dirty RSS: 2251.22 MB
+
+    :apache_only: |
       ---------- Apache processes ----------
       PID    PPID   VMSize    Private  Name
       --------------------------------------

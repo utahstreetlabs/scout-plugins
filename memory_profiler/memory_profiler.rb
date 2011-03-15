d061e48da3ebb87c0b6fa23e881289a767b397a1
Derek Haynes <derek.haynes@gmail.com>
2011-03-15 09:25:58 -0700

    initial solaris-comp

--- memory_profiler/memory_profiler.rb
+++ memory_profiler/memory_profiler.rb
@@ -1,12 +1,10 @@
 class MemoryProfiler < Scout::Plugin
   def build_report
-    
     if solaris?
       solaris_memory
     else
       linux_memory
-    end
-    
+    end   
   end
   
   def linux_memory
@@ -67,8 +65,8 @@ class MemoryProfiler < Scout::Plugin
     prstat =~ /(ZONEID[^\n]*)\n(.*)/
     values = $2.split(' ')
 
-    report_data['Memory Used'] = values[3].to_i
-    report_data['Swap Used']   = values[2].to_i
+    report_data['Memory Used'] = clean_value(values[3])
+    report_data['Swap Used']   = clean_value(values[2])
     
     prtconf = `/usr/sbin/prtconf | grep Memory`    
     
@@ -77,11 +75,11 @@ class MemoryProfiler < Scout::Plugin
     report_data['% Memory Used'] = (report_data['Memory Used'] / report_data['Memory Total'].to_f * 100).to_i
     
     swap = `swap -s`
-    swap =~ /\d+k\sused/
-    swap_used = $&.to_i
-    swap =~ /\d+k\savailable/
-    swap_available = $&.to_i
-    report_data['Swap Total'] = (swap_used+swap_available)/1024
+    swap =~ /\d+[a-zA-Z]\sused/
+    swap_used = clean_value($&)
+    swap =~ /\d+[a-zA-Z]\savailable/
+    swap_available = clean_value($&)
+    report_data['Swap Total'] = swap_used+swap_available
     unless report_data['Swap Total'] == 0   
       report_data['% Swap Used'] = (report_data['Swap Used'] / report_data['Swap Total'].to_f * 100).to_i      
     end
@@ -89,15 +87,35 @@ class MemoryProfiler < Scout::Plugin
     report(report_data)
   end
   
-  # True if on solaris. Only calcuated on the first run (assumes OS does not change).
+  # True if on solaris. Only checked on the first run (assumes OS does not change).
   def solaris?
-    return memory(:solaris) if !memory(:solaris).nil?
-    solaris = false
-    begin
-      solaris = true if `uname` =~ /sun/i
-    rescue
-    end
+    solaris = if @memory.has_key?(:solaris)
+                memory(:solaris) || false
+              else
+                solaris = false
+                begin
+                  solaris = true if `uname` =~ /sun/i
+                rescue
+                end
+              end
     remember(:solaris, solaris)
     return solaris
   end
+  
+  # Ensures solaris memory metrics are in MB. Metrics that don't contain 'T,G,M,or K' are just
+  # turned into integers.
+   def clean_value(value)
+     value = if value =~ /G/i
+       (value.to_f*1024.to_f)
+     elsif value =~ /M/i
+       value.to_f
+     elsif value =~ /K/i
+       (value.to_f/1024.to_f)
+     elsif value =~ /T/i
+       (value.to_f*1024.to_f*1024.to_f)
+     else
+       value.to_f
+     end
+     ("%.1f" % [value]).to_f
+   end
 end
--- memory_profiler/test.rb
+++ memory_profiler/test.rb
@@ -14,7 +14,6 @@ class MemoryProfilerTest < Test::Unit::TestCase
       
       assert res[:errors].empty?
       assert !res[:memory][:solaris]
-      
       assert_equal 6, res[:reports].first.keys.size
       
       r = res[:reports].first
@@ -24,6 +23,16 @@ class MemoryProfilerTest < Test::Unit::TestCase
       assert_equal 0, r["% Swap Used"]
       assert_equal 264, r["Memory Used"]
       assert_equal 1024, r["Memory Total"]
+    end
+    
+    def test_success_linux_second_run
+      # shouldn't run uname again as it is stored in memory
+      @plugin=MemoryProfiler.new(Time.now-60*10,{:solaris=>false},{})
+      @plugin.expects(:`).with("cat /proc/meminfo").returns(File.read(File.dirname(__FILE__)+'/fixtures/proc_meminfo.txt')).once
+      @plugin.expects(:`).with("uname").returns('Linux').never
+
+      res = @plugin.run()
+      assert_equal false,res[:memory][:solaris]
     end
     
     def test_success_solaris
@@ -42,8 +51,42 @@ class MemoryProfilerTest < Test::Unit::TestCase
 
       r = res[:reports].first
       assert_equal 1388, r["Swap Used"]
-      assert_equal 2124, r["Swap Total"]
+      assert_equal 2124.1, r["Swap Total"]
       assert_equal (1388/2124.to_f*100).to_i, r["% Swap Used"]
+      assert_equal 2, r["% Memory Used"]
+      assert_equal 872, r["Memory Used"]
+      assert_equal 32763, r["Memory Total"]
+    end
+    
+    def test_success_solaris_second_run
+      @plugin=MemoryProfiler.new(Time.now-60*10,{:solaris=>true},{})
+      @plugin.expects(:`).with("prstat -c -Z 1 1").returns(File.read(File.dirname(__FILE__)+'/fixtures/prstat.txt')).once
+      @plugin.expects(:`).with("/usr/sbin/prtconf | grep Memory").returns(File.read(File.dirname(__FILE__)+'/fixtures/prtconf.txt')).once
+      @plugin.expects(:`).with("swap -s").returns(File.read(File.dirname(__FILE__)+'/fixtures/swap.txt')).once
+      @plugin.expects(:`).with("uname").returns('SunOS').never
+
+      res = @plugin.run()
+      assert_equal true,res[:memory][:solaris]
+    end
+    
+    def test_success_solaris_with_gb_swap_units
+      @plugin=MemoryProfiler.new(nil,{},{})
+      @plugin.expects(:`).with("prstat -c -Z 1 1").returns(File.read(File.dirname(__FILE__)+'/fixtures/prstat.txt')).once
+      @plugin.expects(:`).with("/usr/sbin/prtconf | grep Memory").returns(File.read(File.dirname(__FILE__)+'/fixtures/prtconf.txt')).once
+      @plugin.expects(:`).with("swap -s").returns(File.read(File.dirname(__FILE__)+'/fixtures/swap_gb.txt')).once
+      @plugin.expects(:`).with("uname").returns('SunOS').once
+
+      res = @plugin.run()
+      
+      assert res[:errors].empty?
+      assert res[:memory][:solaris]
+      
+      r = res[:reports].first      
+      assert_equal 6, r.keys.size
+
+      assert_equal 1388, r["Swap Used"]
+      assert_equal 86016, r["Swap Total"]
+      assert_equal (1388/86016.to_f*100).to_i, r["% Swap Used"]
       assert_equal 2, r["% Memory Used"]
       assert_equal 872, r["Memory Used"]
       assert_equal 32763, r["Memory Total"]

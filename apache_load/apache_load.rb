6fca410fefc7bba3f2630bc5f6303a9273bf52c6
Derek Haynes <derek.haynes@gmail.com>
2011-09-26 14:17:06 -0700

    Added HTTPS support, moved options to file, better error message for bad URLs.

--- apache_load/apache_load.rb
+++ apache_load/apache_load.rb
@@ -1,11 +1,27 @@
+$VERBOSE=false
 class ApacheLoad < Scout::Plugin
   needs "net/http", "uri"
+  
+  OPTIONS=<<-EOS
+    server_url:
+      name: Server Status URL
+      notes: Specify URL of the server-status page to check. Scout requires the machine-readable format of the status page (just add '?auto' to the server-status page URL).
+      default: "http://localhost/server-status?auto"
+  EOS
+  
 	def build_report
     url = URI.parse(option("server_url"))
 		req = Net::HTTP::Get.new(url.path + "?" + url.query.to_s)
-    res = Net::HTTP.start(url.host, url.port) {|http|
-      http.request(req)
+		http = Net::HTTP.new(url.host, url.port)
+		http.use_ssl = url.is_a?(URI::HTTPS)
+    res = http.start() {|h|
+      h.request(req)
     }
+    
+    unless [Net::HTTPOK,Net::HTTPFound].include?(res.class) 
+      return error("Unable to access status page","The server-status page (#{url}) was not accessible:\n\n#{res.class}\n\nPlease ensure the server-status page is configured in your Apache settings.")
+    end
+    
 		values = {}
 		res.body.split("\n").each do |item|
 			k, v = item.split(": ")

803b77e72055de619f3553048caf61f4ae66e615
Derek Haynes <derek.haynes@gmail.com>
2011-10-03 11:20:08 -0700

    Reworked HAProxy monitoring: only monitoring one backend, more accurate req/sec, error reporting, status reporting

---
+++ haproxy_monitoring/haproxy_monitoring.rb
@@ -0,0 +1,60 @@
+class HaproxyMonitoring < Scout::Plugin
+
+  if RUBY_VERSION < "1.9"
+    needs 'fastercsv'
+  else
+    # typically, avoid require. In this case we can't use needs' deferred loading because we need to alias CSV
+    require 'csv'
+    FasterCSV=CSV
+  end
+  needs 'open-uri'
+
+  OPTIONS=<<-EOS
+  uri:
+    name: URI
+    notes: URI of the haproxy CSV stats url. See the 'CSV Export' link on your haproxy stats page.
+    default: http://yourdomain.com/;csv
+  proxy:
+    notes: The name of the proxy to monitor. Proxies are typically listed in the haproxy.cfg file.
+  user:
+    notes: If protected under basic authentication provide the user name.
+  password:
+    notes: If protected under basic authentication provide the password.   
+  EOS
+
+  def build_report
+
+    if option(:uri).nil?
+      return error('URI to HAProxy Stats Required', "It looks like the URI to the HAProxy stats page (in csv format) hasn't been provided. Please enter this URI in the plugin settings.")
+    end
+    proxy = option(:proxy)
+    possible_proxies = []
+    proxy_found = false
+    begin
+      FasterCSV.parse(open(option(:uri),:http_basic_authentication => [option(:user),option(:password)]), :headers => true) do |row|
+        if row["svname"] == 'FRONTEND' || row["svname"] == 'BACKEND'
+          possible_proxies << row["# pxname"]
+          next unless proxy.to_s.strip.downcase == row["# pxname"].downcase
+          proxy_found = true
+          counter(:requests, row['stot'].to_i, :per => :minute)
+          counter(:errors_req, row['ereq'].to_i, :per => :minute) if row['ereq']     
+          counter(:errors_conn, row['econ'].to_i, :per => :minute) if row['econ']       
+          counter(:errors_resp, row['eresp'].to_i, :per => :minute) if row['eresp']  
+          report(:proxy_up=>%w(UP OPEN).find {|s| s == row['status']} ? 1 : 0)
+        end
+      end
+    rescue OpenURI::HTTPError
+      raise if $!.message != '404 Not Found'
+      return error("Unable to find the stats page", "The stats page could not be found at: #{option(:uri)}. If you are providing a user and password please ensure those are correct.")
+    rescue FasterCSV::MalformedCSVError
+      return error('Error accessing stats page', "The plugin encountered an error attempting to access the stats page (in CSV format) at: #{option(:uri)}. The exception: #{$!.message}\n#{$!.backtrace}")
+    end
+    if proxy.nil?
+      error('Proxy name required',"The name of the proxy to monitor must be provided in the plugin settings. The possible proxies to monitor:\n\n#{possible_proxies.map { |p| "* #{p}"}.join('\n')}")
+    elsif !proxy_found
+      error('Proxy not found',"The proxy '#{proxy}' was not found. The possible proxies to monitor:\n\n#{possible_proxies.map { |p| "* #{p}"}.join('\n')}")
+      
+    end
+  end
+  
+end
---
+++ haproxy_monitoring/test.rb
@@ -0,0 +1,102 @@
+require File.expand_path('../../test_helper.rb', __FILE__)
+require File.expand_path('../haproxy_monitoring.rb', __FILE__)
+
+require 'open-uri'
+class HaProxyTest < Test::Unit::TestCase
+
+  def teardown
+    FakeWeb.clean_registry
+  end
+
+  def test_two_runs
+    # Stub the plugin instance where necessary and run
+    # @plugin=PluginName.new(last_run, memory, options)
+    #                        date      hash    hash
+    uri='http://fake'
+    proxy = 'rails'
+    time = Time.now
+    Timecop.travel(time-60*10) do
+      FakeWeb.register_uri(:get, uri, :body => File.read(File.dirname(__FILE__)+'/fixtures/sample.csv'))
+      @plugin=HaproxyMonitoring.new(nil,{},{:uri=>uri, :proxy => proxy})
+      res = @plugin.run()
+      assert_equal 120350789, res[:memory]["_counter_requests"][:value]
+      assert_equal 10860, res[:memory]["_counter_errors_req"][:value]
+      assert_equal 10, res[:memory]["_counter_errors_conn"][:value]
+      assert_equal 20, res[:memory]["_counter_errors_resp"][:value]
+      assert_equal 1, res[:reports].first[:proxy_up]
+      first_run_memory = res[:memory]
+      
+      # now - 10 minutes later
+      Timecop.travel(time) do
+        FakeWeb.register_uri(:get, uri, :body => File.read(File.dirname(__FILE__)+'/fixtures/sample_second_run.csv'))
+        @plugin=HaproxyMonitoring.new(time-60*10,first_run_memory,{:uri=>uri, :proxy => 'rails'})
+        res = @plugin.run()
+        assert_in_delta 10, res[:reports].first[:requests], 0.001
+        assert_in_delta 1, res[:reports].find { |e| e[:errors_req]}[:errors_req], 0.001
+        assert_in_delta 0, res[:reports].first[:errors_conn], 0.001
+        assert_in_delta 0.1, res[:reports].find { |e| e[:errors_resp]}[:errors_resp], 0.001      
+        assert_equal 0, res[:reports].find { |e| e[:proxy_up]}[:proxy_up]
+      end # 2nd run
+    end # travel
+  end
+  
+  def test_no_proxy_provided
+    uri='http://fake' # output comes from http://demo.1wt.eu/;csv
+    FakeWeb.register_uri(:get, uri, :body => File.read(File.dirname(__FILE__)+'/fixtures/sample.csv'))
+    @plugin=HaproxyMonitoring.new(nil,{},{:uri=>uri})
+    res = @plugin.run()
+    error = res[:errors].first
+    assert_equal "Proxy name required", error[:subject]
+  end
+  
+  def test_proxy_not_found
+    uri='http://fake' # output comes from http://demo.1wt.eu/;csv
+    FakeWeb.register_uri(:get, uri, :body => File.read(File.dirname(__FILE__)+'/fixtures/sample.csv'))
+    @plugin=HaproxyMonitoring.new(nil,{},{:uri=>uri, :proxy => 'invalid'})
+    res = @plugin.run()
+    error = res[:errors].first
+    assert_equal "Proxy not found", error[:subject]
+  end
+
+  def test_invalid_csv
+    uri='http://fake'
+    FakeWeb.register_uri(:get, uri, :body => File.read(File.dirname(__FILE__)+'/fixtures/invalid.csv'))
+    @plugin=HaproxyMonitoring.new(nil,{},{:uri=>uri,:proxy => 'rails'})
+
+    res = @plugin.run()
+    assert_equal 0, res[:reports].size
+    assert_equal 1, res[:errors].size
+    assert_equal "Error accessing stats page", res[:errors].first[:subject]
+  end
+  
+  def test_blank_uri
+    @plugin=HaproxyMonitoring.new(nil,{},{:uri=>nil})
+
+    res = @plugin.run()
+    assert_equal 0, res[:reports].size
+    assert_equal 1, res[:errors].size
+    assert_equal "URI to HAProxy Stats Required", res[:errors].first[:subject]
+  end
+  
+  def test_invalid_basic_auth
+    uri_invalid_auth = "http://user:invalid@example.com/secret"
+    FakeWeb.register_uri(:get, uri_invalid_auth, :body => "Unauthorized", :status => ["401", "Unauthorized"])
+
+    @plugin=HaproxyMonitoring.new(nil,{},{:uri=>uri_invalid_auth, :user => 'user', :password => 'invalid'})
+    assert_raises OpenURI::HTTPError do
+      res = @plugin.run()
+    end
+  end
+  
+  def test_valid_basic_auth
+    uri_no_auth = "http://example.com/secret"
+    uri = "http://user:pass@example.com/secret"
+    FakeWeb.register_uri(:get, uri_no_auth, :body => "Unauthorized", :status => ["401", "Unauthorized"])
+    FakeWeb.register_uri(:get, uri, :body => File.read(File.dirname(__FILE__)+'/fixtures/sample.csv'))
+    
+    @plugin=HaproxyMonitoring.new(nil,{},{:uri=>uri_no_auth, :user => 'user', :password => 'pass', :proxy => 'rails'})
+    res = @plugin.run()
+    assert res[:errors].empty?
+    assert res[:memory].any?
+  end
+end

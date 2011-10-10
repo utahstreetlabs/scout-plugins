090aca12a97cbc9ee6fbaf9b00cdabd5beea483a
Lucas Parry <lucas@envato.com>
2011-10-10 11:48:32 +1100

    More configurable and handles connection errors.
    * Replication path needs to be configurable for multi-core setups.
    * Refactored the #build_report to use a few smaller methods
    * Added some error handling for connection errors, so that the error we report to scout is more meaningful.

--- solr_replication/solr_replication.rb
+++ solr_replication/solr_replication.rb
@@ -2,21 +2,43 @@ class SolrReplication < Scout::Plugin
   needs 'open-uri'
 
   OPTIONS=<<-EOS
-  master:
-    default: http://192.168.0.1:8983
-  slave:
-    default: http://localhost:8765
+  master_ip:
+    default: 192.168.0.1
+  master_port:
+    default: 8983
+  slave_ip:
+    default: localhost
+  slave_port:
+    default: 8765
+  replication_path:
+    default: /solr/admin/replication/index.jsp
   EOS
 
   def build_report
-    replication_path = '/solr/admin/replication/index.jsp'
-    rex = /Generation: (\d+)/
-    master = open(option(:master)+replication_path).read.match(rex)[1]
-    slave = open(option(:slave)+replication_path).read.match(rex)[1]
-    if master and slave
-      report 'delay' => master.to_i - slave.to_i
+    master_position = position_for(master_host)
+    slave_position = position_for(slave_host)
+    if master_position and slave_position
+      report 'delay' => master_position.to_i - slave_position.to_i
     else
-      error "Incorrect values found master:#{master} slave:#{slave}"
+      error "Incorrect values found master:#{master_position.inspect} slave:#{slave_position.inspect}"
     end
+  end
+
+  private
+
+  def position_for(host)
+    generation_regex = /Generation: (\d+)/
+    open(host).read.match(generation_regex)[1]
+  rescue => e
+    error "Error connecting to #{host}: #{e.message}"
+    nil
+  end
+
+  def master_host
+    "http://#{option(:master_ip)}:#{option(:master_port)}#{option("replication_path")}"
+  end
+
+  def slave_host
+    "http://#{option(:slave_ip)}:#{option(:slave_port)}#{option("replication_path")}"
   end
 end

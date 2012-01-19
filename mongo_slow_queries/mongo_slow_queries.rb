3cb7febc8b5cf11cde1286c8a52b06693b06f8da
Derek Haynes <derek.haynes@gmail.com>
2012-01-19 09:06:33 -0800

    allowing slave connections

--- mongo_slow_queries/mongo_slow_queries.rb
+++ mongo_slow_queries/mongo_slow_queries.rb
@@ -58,7 +58,7 @@ class ScoutMongoSlow < Scout::Plugin
       threshold = threshold_str.to_i
     end
             
-    db = Mongo::Connection.new(server, option("port").to_i).db(database)                 
+    db = Mongo::Connection.new(server, option("port").to_i,:slave_ok=>true).db(database)                 
     db.authenticate(option(:username), option(:password)) if !option(:username).to_s.empty?
     enable_profiling(db)
 

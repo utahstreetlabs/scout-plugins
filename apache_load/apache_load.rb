6f4a54bea9e2e8e3e37b70985cba470154c19dab
Derek Haynes <derek.haynes@gmail.com>
2012-02-21 10:15:01 -0800

    Added Busy+Idle Workers, cleaned up counter behavior

--- apache_load/apache_load.rb
+++ apache_load/apache_load.rb
@@ -28,18 +28,7 @@ class ApacheLoad < Scout::Plugin
 			values[k] = v
 		end
 		total_accesses = values['Total Accesses'].to_i
-		if (memory(:last_run_total_accesses) && memory(:last_run_time))
-			accesses_since_last_run = total_accesses - memory(:last_run_total_accesses)
-			seconds_since_last_run = Time.now - memory(:last_run_time)
-		else
-			accesses_since_last_run = total_accesses
-			seconds_since_last_run = values['Uptime'].to_i
-		end
-		
-		current_accesses_per_second = accesses_since_last_run / seconds_since_last_run
-		
-		report(:current_load => current_accesses_per_second)
-    remember(:last_run_time => Time.now)
-    remember(:last_run_total_accesses => total_accesses)
+		counter(:current_load, total_accesses, :per => :second)
+		report(:busy_workers => values['BusyWorkers'].to_i, :idle_workers => values['IdleWorkers'].to_i)
   end
 end

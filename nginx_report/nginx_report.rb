dfc43a7a8db4e25a30797cdbdc87526740739469
Jean-Luc Geering <jlgeering.12@gmail.com>
2012-03-19 17:20:01 +0100

    tabs to spaces

--- nginx_report/nginx_report.rb
+++ nginx_report/nginx_report.rb
@@ -8,23 +8,23 @@ class NginxReport < Scout::Plugin
     default: "http://127.0.0.1/nginx_status"
   EOS
 
-  def build_report	
-	  url = option(:url) || 'http://127.0.0.1/nginx_status'
+  def build_report  
+    url = option(:url) || 'http://127.0.0.1/nginx_status'
 
-  	total, requests, reading, writing, waiting = nil
+    total, requests, reading, writing, waiting = nil
 
-  	open(url) {|f|
-  	  f.each_line do |line|
-  		total = $1 if line =~ /^Active connections:\s+(\d+)/
-  		if line =~ /^Reading:\s+(\d+).*Writing:\s+(\d+).*Waiting:\s+(\d+)/
-  		  reading = $1
-  		  writing = $2
-  		  waiting = $3
-  		end
-  		requests = $3 if line =~ /^\s+(\d+)\s+(\d+)\s+(\d+)/  
-  	  end
-  	}
-	
-	  report({:total => total, :reading => reading, :writing => writing, :waiting => waiting, :requests => requests})
+    open(url) {|f|
+      f.each_line do |line|
+      total = $1 if line =~ /^Active connections:\s+(\d+)/
+      if line =~ /^Reading:\s+(\d+).*Writing:\s+(\d+).*Waiting:\s+(\d+)/
+        reading = $1
+        writing = $2
+        waiting = $3
+      end
+      requests = $3 if line =~ /^\s+(\d+)\s+(\d+)\s+(\d+)/  
+      end
+    }
+
+    report({:total => total, :reading => reading, :writing => writing, :waiting => waiting, :requests => requests})
   end
 end
